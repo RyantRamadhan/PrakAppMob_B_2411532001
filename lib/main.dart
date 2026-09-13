6af8497a414cdefb157abb3a1b45102541a814cd
unknown <aryanta2105@gmail.com>
2026-09-13 18:48:20 +0700

    Praktikum 1

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'user_registration_form.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Praktikum 3 - Form Registrasi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const UserRegistrationForm(),
+    );
+  }
+}
